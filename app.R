d1eb7463efcfd380e20a97d56d8d683d90541379
murattarakci <tarakci@rsm.nl>
2020-05-09 17:16:12 +0200

    again

--- app.R
+++ app.R
@@ -296,8 +296,11 @@ services<-argonPage(
         argonRow(
           argonColumn(
             width = 12,
-           tag$iframe(src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FAmsterdam&amp;src=cWF2bzRyaDEzbjY0YTViZTBuNjF1N3A4ZWdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=NXM1b2pncXRvNW52djQxOW4xdTloam82YTRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23F6BF26&amp;color=%23F4511E&amp;showTitle=0&amp;showNav=1&amp;showDate=1&amp;showPrint=0&amp;showTabs=1&amp;showCalendars=0") 
-          #tags$script(src="https://localendar.com/public/managementseminars?include=Y&dynamic=Y")
+        #tags$iframe(src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FAmsterdam&amp;src=cWF2bzRyaDEzbjY0YTViZTBuNjF1N3A4ZWdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=NXM1b2pncXRvNW52djQxOW4xdTloam82YTRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23F6BF26&amp;color=%23F4511E&amp;showTitle=0&amp;showNav=1&amp;showDate=1&amp;showPrint=0&amp;showTabs=1&amp;showCalendars=0", style="border-width:0", width="800", height="600", frameborder="0", scrolling="no")  
+          
+        tags$iframe(src="https://calendar.google.com/calendar?cid=cWF2bzRyaDEzbjY0YTViZTBuNjF1N3A4ZWdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ")
+        
+        #tags$script(src="https://localendar.com/public/managementseminars?include=Y&dynamic=Y")
                        
           )
                     
