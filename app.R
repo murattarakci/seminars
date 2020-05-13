37c495fe6128c999846bd4805664b6e3edcf4776
murattarakci <tarakci@rsm.nl>
2020-05-13 10:06:19 +0200

    Fixed the happening typo

--- app.R
+++ app.R
@@ -80,7 +80,7 @@ nav<- tags$nav(class="navbar navbar-expand-lg navbar-light bg-lignt",
                                    )),
                                  tags$ul(
                                    class="navbar-nav ml-auto",
-                                   tags$li(class="nav-item",tags$a("Hapenning soon",href="index.html",class="nav-link")),
+                                   tags$li(class="nav-item",tags$a("Happening soon",href="index.html",class="nav-link")),
                                    tags$li(class="nav-item ",tags$a("Calendar view",href="services.html",class="nav-link")
                                            #argonDropdown(name="Services",argonDropdownItem(name="ING Vysya",src = 'https://agilenow.ai/ING/'),
                                            #argonDropdownItem(name="Demo",src = 'https://agilenow.ai/demo/'))
